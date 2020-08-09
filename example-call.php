f0afcefdf33f833e0ea9c7b577cb1d97e6cf26d4
Jon Bennett <jon@boxedred.com>
2020-08-09 18:18:12 +0100

    function and example use

---
+++ example-call.php
@@ -0,0 +1,10 @@
+<?php
+// Example to use featured image if applicable, if not call the function to return image uploaded to post or placeholder //
+if(has_post_thumbnail()) {
+  $imgurl = get_the_post_thumbnail_url('','full');
+} else {
+  $imgurl = find_first_img_attachment();
+}
+// returns an image url either featured image, uploaded image or placeholder //
+echo $imgurl;
+?>
---
+++ image-catch-function.php
@@ -0,0 +1,20 @@
+<?php
+// add to functions.php //
+function find_first_img_attachment() {
+  global $post, $posts;
+  $first_img = '';
+  ob_start();
+  ob_end_clean();
+  // Find all images  //
+  $output = preg_match_all('/<img.+?src=[\'"]([^\'"]+)[\'"].*?>/i', $post->post_content, $matches);
+  // Check if post includes an image and has value //
+  if(isset($matches[1][0])) {
+    $first_img = $matches[1][0];
+  }
+  // fallback placeholder - nothing found //
+  if(empty($first_img)) {
+    $first_img = "/images/image_placeholder_here.png";
+  }
+  return $first_img;
+}
+?>
